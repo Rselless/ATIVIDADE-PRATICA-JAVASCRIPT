01a17f0ccd08479f3f91acf95676467f8fcdd853
Rselless <selles.rafael27@gmail.com>
2025-05-26 17:31:08 -0300

    atividades var-ope-2

--- atividade4.js
+++ atividade4.js
@@ -10,4 +10,4 @@ numero4=leia.questionFloat(`Insira o quarto valor: `);
 
 diferenca1=((numero1*numero2)-(numero3*numero4))
 diferenca2=((numero1*numero2)-(numero3a*numero4))
-console.log(`As Diferenças são: ${diferenca1.toFixed(1)} e ${diferenca2.toFixed(1)}`)
+console.log(`As diferenças dos produtos de n1 e n2 subtraidos dos produtos de n3 ou -n3 e n4 são: ${diferenca1.toFixed(1)} e ${diferenca2.toFixed(1)}`)
